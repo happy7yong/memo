45cf15abe544116c78e69248d149b1cb80d92a0a
chaeyoung <happy7yong83@gmail.com>
2024-08-14 15:56:56 +0900

    @repository 추가

--- src/main/java/com/meta/memo/repository/MemoRepository.java
+++ src/main/java/com/meta/memo/repository/MemoRepository.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -15,6 +16,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
+@Repository
 @Component
 public class MemoRepository {
 
