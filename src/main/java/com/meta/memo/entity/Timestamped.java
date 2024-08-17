a49deec3ae3eea99254348642b97578042bc6058
yongchaeyoung <happy7yong83@gmail.com>
2024-08-18 00:43:04 +0900

    feat : 시간조회 기능 추가

---
+++ src/main/java/com/meta/memo/entity/Timestamped.java
@@ -0,0 +1,22 @@
+package com.meta.memo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
+
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Timestamped {
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createAt;
+
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
+}
